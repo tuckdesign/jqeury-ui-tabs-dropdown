08a61bd2d4798e288f52d8059dc09a14a45e458f
tuckdesign <contact@tuckdesign.com>
2025-01-18 13:12:24 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jqeury-ui-tabs-dropdown
-Plugin for jQuery UI for responsvie tabs widget
+Plugin for jQuery UI for responsive tabs widget. It will show dropdown instead of tabs list when on mobile (depends on @media query in css - you can customize)
+
+Requires jQuery UI Selectmenu Widget.
+
+Uses CSS @media to choose will regular tabs show or dropdow.
+
---
+++ tabs-dropdown.js
@@ -0,0 +1,43 @@
+(function($) {
+    var methods = {
+        init: function() {
+            return this.each(function() {
+                var $tabsDropdown = $(this);
+                $tabsDropdown.tabs();
+                let $tablist = $tabsDropdown.find('ul[role=tablist]');
+                $tablist.addClass('tabs-dropdown-ul');
+                var $dropdown = $('<select></select>');
+                var count = 0;
+                $tablist.find('li').each(function() {
+                    let a = $(this).find('a');
+                    let option = $('<option></option>');
+                    option.attr('value', count);
+                    option.html(a.html());
+                    $dropdown.append(option);
+                    count++;
+                })
+                $dropdown.insertAfter($tablist);
+                $dropdown.selectmenu({
+                    change: function(event) {
+                        let tab = $(event.target).find(":selected").val();
+                        $tabsDropdown.tabs( "option", "active", tab );
+                    }
+                }).selectmenu( "menuWidget" );
+                $dropdown.selectmenu('widget').addClass('tabs-dropdown-dropdown');
+                //initiate jQuery UI Tabs
+                $tabsDropdown.on( "tabsactivate", function( event, ui ) {
+                    $dropdown.val($tabsDropdown.tabs( "option", "active"));
+                });
+            });
+        }
+    };
+    $.fn.tabsDropdown = function(method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on $.tabsDropdown');
+        }
+    };
+})(jQuery);
